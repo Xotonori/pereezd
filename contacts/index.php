6e3c8574e78614aa6270e2a7095a3671fe46e426
Dmitrieva.GG <dmitrieva.gg@parma.ru>
2019-09-22 23:29:33 +0300

    contacts

---
+++ contacts/index.php
@@ -0,0 +1,50 @@
+<?php $city = 'Гатчино'; ?>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
+    <link rel="canonical" href="https://pereezdy-spb.ru/kvartirnyy-pereezd/"/>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
+    <img class="sofa" src="/images/header/sofa.png" alt="диван">
+    <img class="man" src="/images/header/man.png" alt="грузчик">
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
+    <h1>Контактная информация</h1>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+    <div class="container contacts">
+        <div class="row">
+            <div class="col-12">
+                <p class="paragraph">г. <b>Санкт-Петербург</b>, Левашовский пр-кт, д. 12.<br>Телефон:<br>
+                    <a class="tyu_0" href="tel:+78129828858">+7 (812) 982-88-58</a><br>
+                    <a class="tyu_0" href="tel:+79618018881">+7 (961) 801-88-81</a>
+                </p>
+                <div class="map">
+                    <img class="wp-image-346 size-full" src="/images/map_piter.png" alt="Схема проезда Санкт-Петербург">
+                    <p class="caption-text">Схема проезда<br>
+                        <a href="http://maps.yandex.ru/-/CVbxJUpi" target="_blank">Посмотреть на Яндекс.Картах</a>
+                    </p>
+                </div>
+                <div class="work-time">
+                    <p>Заявки принимаются по телефонам в рабочее время:</p>
+                    <ul class="marker">
+                        <li>Пн-Пт: 09:00-21:00;</li>
+                        <li>Сб, Вс: 10:00-20:00;</li>
+                        <li>Сами переезды осуществляются 24 часа в сутки.</li>
+                    </ul>
+                    <p>А также круглосуточно ждем ваших
+                        <a title="онлайн заявок" href="/zakaz/">онлайн заявок</a>
+                        и писем на <a href="mailto:info@pereezdy-spb.ru">info@pereezdy-spb.ru</a>.
+                    </p>
+                </div>
+                <h2 class="text-center">Реквизиты организации</h2>
+                <p>
+                    ИНН 7720582274<br>
+                    КПП 771601001<br>
+                    ОГРН 5077746689873<br>
+                    ОКВЭД 63.40 93.05<br>
+                    ОКПО 99709347<br>
+                    Банк АКБ МОСОБЛБАНК ОАО г. Москва<br>
+                    р/с 40702810402700000812<br>
+                    к/с 30101810900000000521 в ОПЕРУ Московского ГТУ Банка России<br>
+                    БИК 044525521
+                </p>
+            </div>
+        </div>
+    </div>
+<?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
