abc00b0102578351e19f6f2ad7d786872713ce51
Dmitrieva.GG <dmitrieva.gg@parma.ru>
2019-09-22 17:09:28 +0300

    start reference

--- reference/index.php
+++ reference/index.php
@@ -1,92 +1,32 @@
+<?php $city = 'Гатчино'; ?>
 <?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
-<link rel="canonical" href="https://pereezdy-spb.ru/kvartirnyy-pereezd/"/>
+    <link rel="canonical" href="https://pereezdy-spb.ru/kvartirnyy-pereezd/"/>
 <?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
-<h1>Отзывы клиентов</h1>
+    <img class="sofa" src="/images/header/sofa.png" alt="диван">
+    <img class="man" src="/images/header/man.png" alt="грузчик">
 <?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
-
-<main>
-    <div class="container zakaz">
+    <h1>Отзывы клиентов</h1>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+    <div class="container client-reviews">
         <div class="row">
-            <div class="col-12 paragraph">
-                <p>Вы можете заказать переезд не только по телефону, но и с помощью онлайн-заявки. При заполнении полей рекомендуется указывать всю известную информацию о будущем переезде, поскольку это поможет нашим менеджерам точнее рассчитать сроки выполнения работы и общую стоимость услуг.</p>
-                <p>Оформить заказ переезда на сайте можно в любое удобное для вас время. После отправки заполненной формы вам больше не придется ничего делать: получив заявку, наши специалисты сами свяжутся с вами. Такой заказ ни к чему вас не обязывает. После получения бесплатной консультации вы сможете решить, будете ли пользоваться нашими услугами.</p>
-            </div>
-            <div class="form-block mx-auto">
-                <!--                <form action="https://pereezdy-spb.ru/zakaz/#usermessage2b" id="cforms2form" class="js-modal">-->
-                <form action="https://pereezdy-spb.ru/zakaz/#usermessage2b" id="cforms2form" method="post" class="js-modal">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="cf2_field_1">ФИО</label>
-                            <input type="text" class="form-control" name="cf2_field_1" data-id="name">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_2">Телефон</label>
-                            <input type="tel" class="form-control" name="cf2_field_2" data-id="tel" placeholder="+79210000000">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_3">E-mail</label>
-                            <input type="tel" class="form-control" name="cf2_field_3" data-id="email">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_4">Откуда переезжаем? город, ул, дом.</label>
-                            <input type="tel" class="form-control" name="cf2_field_4" data-id="address_from">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_5">Куда переезжаем? город, ул, дом.</label>
-                            <input type="tel" class="form-control" name="cf2_field_5" data-id="address_to">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_6">Описание перевозимых вещей</label>
-                            <textarea class="form-control" rows="3" name="cf2_field_6" data-id="stuff"></textarea>
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_7">Упаковываем мебель и орг. технику</label>
-                            <input type="tel" class="form-control" name="cf2_field_7" data-id="wrap_up">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_8">Откуда переезжаем какой этаж и наличие лифта</label>
-                            <input type="tel" class="form-control" name="cf2_field_8" data-id="floor_from">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_9">Куда переезжаем какой этаж и наличие лифта</label>
-                            <input type="tel" class="form-control" name="cf2_field_9" data-id="floor_to">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_10">Число, месяц и время переезда</label>
-                            <input type="tel" class="form-control" name="cf2_field_10" data-id="date">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_11">Расчет наличный или безналичный</label>
-                            <input type="tel" class="form-control" name="cf2_field_11" data-id="payment">
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-group">
-                            <label for="cf2_field_12">Дополнительная информация</label>
-                            <textarea class="form-control" rows="3" name="cf2_field_12" data-id="additional"></textarea>
-                            <small class="text-danger"></small>
-                        </div>
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" data-id="agree" name="p5">
-                            <label class="form-check-label" for="exampleCheck1">Я принимаю <a href="/soglasie_na_sbor_dannih.pdf" target="blank">соглашение на обработку персональных данных</a></label>
-                        </div>
+            <div id="clients-reviews" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#clients-reviews" data-slide-to="0" class="active"></li>
+                    <li data-target="#clients-reviews" data-slide-to="1"></li>
+                    <li data-target="#clients-reviews" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block" src="/images/client-reviews/otzyv_gbu-150x150.jpg" alt="First slide">
                     </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="send-button">Отправить</button>
+                    <div class="carousel-item">
+                        <img class="d-block" src="/images/client-reviews/otz5561-150x150.jpg" alt="Second slide">
                     </div>
-                </form>
+                    <div class="carousel-item">
+                        <img class="d-block" src="/images/client-reviews/shag-150x150.jpg" alt="Second slide">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</main>
 <?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
-
