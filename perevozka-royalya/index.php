f28b3a7670fa1873eda255463e99043817592e96
Xotonori <xotonori29011991@gmail.com>
2019-09-25 19:36:47 +0300

    сделал страницу перевозка рояля

---
+++ perevozka-royalya/index.php
@@ -0,0 +1,327 @@
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
+<meta name="description" itemprop="description" content="Бережная перевозка рояля от компании Переезд Без Хлопот в Санкт-Петербурге"/>
+<meta name="keywords" itemprop="keywords" content="" />
+<title>Перевозка пианино</title>
+<link rel="canonical" href="https://pereezdy-spb.ru/perevozka-royalya"/>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
+<img class="sofa" src="/images/header/sofa.png" alt="диван">
+<img class="man" src="/images/header/man.png" alt="грузчик">
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
+<h1>Перевозка рояля</h1>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+
+<main class="perevozka-royalya">
+    <!--////PAGE-DESCRIPTION///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="js-long-text container">
+        <div class="row">
+            <p class="col-12 paragraph">
+                Перевозка рояля по Санкт-Петербургу и области – своего рода уникальная транспортная операция. Это дорогой музыкальный инструмент, требующий максимально деликатного и профессионального обращения. Наша компания имеет солидный опыт в перевозке таких сложных грузов. Перевозка габаритных музыкальных инструментов производится только опытными специалистами, с использованием профессиональных упаковочных материалов. Сложность заключается в том, что инструмент имеет большой вес и относительно большие габаритные размеры, что затрудняет их перемещение в пределах здания. Доверьтесь нам, и мы сделаем все, чтобы перевозка роялей в Санкт-Петербурге прошла быстро, надежно и дешево для вас.
+            </p>
+            <div class="col d-flex justify-content-end">
+                <button class="js-podrobno">
+                    <span>Подробно</span>
+                    <img src="/images/header/city-triangle.png" alt="треугольник">
+                </button>
+            </div>
+        </div>
+    </div>
+    <!--////range-of-services///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-range-of-services">
+        <h2 class="_header-3 text-center mb-4">Мы оказываем <span class="important">полный</span> спектр услуг</h2>
+        <div class="row">
+            <div class="col-md-4 d-flex align-content-center range-of-services">
+                <div class="h-100 d-inline-block check-mark">
+                    <img src="/images/gl.png" alt="галочка" class="mt-2">
+                </div>
+                <div class="service d-inline-block">
+                    Погрузка/разгрузка роялей
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-content-center range-of-services">
+                <div class="h-100 d-inline-block check-mark">
+                    <img src="/images/gl.png" alt="галочка" class="mt-2">
+                </div>
+                <div class="service d-inline-block">
+                    Перевозка роялей
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-content-center range-of-services">
+                <div class="h-100 d-inline-block check-mark">
+                    <img src="/images/gl.png" alt="галочка" class="mt-2">
+                </div>
+                <div class="service d-inline-block">
+                    Доставка новых роялей из магазинов
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-content-center range-of-services">
+                <div class="h-100 d-inline-block check-mark">
+                    <img src="/images/gl.png" alt="галочка" class="mt-2">
+                </div>
+                <div class="service d-inline-block">
+                    Ответственное хранение на складе
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-content-center range-of-services">
+                <div class="h-100 d-inline-block check-mark">
+                    <img src="/images/gl.png" alt="галочка" class="mt-2">
+                </div>
+                <div class="service d-inline-block">
+                    Разборка и сборка роялей при необходимости
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-content-center range-of-services">
+                <div class="h-100 d-inline-block check-mark">
+                    <img src="/images/gl.png" alt="галочка" class="mt-2">
+                </div>
+                <div class="service d-inline-block">
+                    Организация переездов под ключ
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--////garant///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-garant">
+        <h3 class="_header-3 text-center mb-4"><span class="important">Гарантируем</span> низкие цены</h3>
+        <div class="row">
+            <div class="h-100 col-lg-3 d-flex justify-content-lg-start justify-content-center">
+                <img src="/images/garant.png" alt="гарантия">
+            </div>
+            <p class="garant col-lg-9">
+                Если Вы желаете перевезти рояль недорого, обращайтесь! В нашем распоряжении – собственный парк специализированного автотранспорта, штат опытных грузчиков, сотни успешно выполненных заказов по транспортировке музыкальных инструментов. Именно поэтому мы не только гарантируем высокое качество работ, но и предлагаем действительно выгодные цены.
+            </p>
+        </div>
+    </div>
+
+
+    <!--////garant///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-kv-gorod">
+        <h2 class="_header-3 text-center mb-4">
+            Перевозка
+            <span class="important"> Рояля </span>
+            с грузчиками<br>
+            <span class="red-bold">
+                от <span class="big-red">5000</span> руб.
+            </span>
+        </h2>
+        <div class="row">
+            <div class="col-lg-3 d-flex align-items-center flex-lg-wrap align-items-stretch mb-lg-0 mb-3">
+                <div class="col-lg-12 d-flex justify-content-center justify-content-lg-start wrap-check-mark">
+                    <div class="h-100 d-inline-block check-mark d-flex align-content-center">
+                        <img src="/images/gl.png" alt="галочка" class="mr-4">
+                    </div>
+                    <div class="d-inline-block">
+                        Газель фургон 10 м3
+                    </div>
+                </div>
+                <div class="col-lg-12 d-flex justify-content-center justify-content-lg-start wrap-check-mark">
+                    <div class="h-100 d-inline-block check-mark d-flex align-content-center">
+                        <img src="/images/gl.png" alt="галочка" class="mr-4">
+                    </div>
+                    <div class="d-inline-block">
+                        2 грузчика
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-9 d-flex align-items-center">
+                Перевозка роялей осуществляется только квалифицированными специалистами, особым автотранспортом – грузовыми фургонами, оснащенными системой крепления груза и гидроманипулятором. Перевозка музыкального инструмента– это ответственная работа, с которой мы справляемся на «отлично». Заказчики могут быть совершенно спокойны - перевозка рояля будет проведена вовремя, с соблюдением всех требований безопасности.
+            </div>
+        </div>
+    </div>
+    <!--////Benefits/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-benefits">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Нам можно <span class="important">доверить</span> свой <span class="important">переезд</span></h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 benefit1 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive1.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Доступные цены на квартирные переезды по Санкт-Петербургу</p>
+                    <p class="hint col-12 text-center">Мы покупаем упаковку оптовыми партиями, используем машины из
+                        автопарка компании и не допускаем лишних расходов. У нас – дешевле!</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit2 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive2.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Подача транспорта в установленное время</p>
+                    <p class="hint col-12 text-center">Наши сотрудники ценят ваше время,поэтому всегда приезжают
+                        вовремя и работают оперативно. Можно вызвать транспорт даже ночью.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit3 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive3.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Заботливый подход к вещам и мебели клиента.</p>
+                    <p class="hint col-12 text-center">Тщательно упакуем каждый предмет от посуды до шкафа-купе,
+                        бережно спустим, погрузими закрепим в кузове автомобиля спец фиксаторами.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit4 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive4.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Положительные рекомендации от заказчиков</p>
+                    <p class="hint col-12 text-center">На сайте Вы узнаете отзывы людей, которые уже
+                        воспользовались нашими услугами. <a href="/articles" class="blue-ancor">Почитайте</a> и убедитесь, что нам можно доверять.
+                        Посмотрите <a href="/video_reference" class="blue-ancor">видео отзывы</a> и увидите модель Вашего будущего переезда.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--////RECOMMENDATION/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="recommendation container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Нас<span class="important"> рекомендуют</span></h2>
+            </div>
+        </div>
+    </div>
+    <div class="bd-example">
+        <div id="recommendationCaption1" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img src="/images/perevozka-royalya/royal1.jpg" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Карпов Вячеслав</h5>
+                        <p class="text-left text-lg-center">
+                            Такую машину, как у вас, наверное, очень трудно найти, ведь перевозка рояля – это событие не частое, маловостребованное. Очень аккуратно погрузили, закрепили и закрыли в фургоне от солнца и дождя, который у нас может пойти в любой момент, и перевезли.
+                        </p>
+                        <div class="transportation-parameters">
+                            <p class="param">
+                                <i class="b-param">Заказывал перевозку рояля</i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-royalya/royal2.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Чайка Олег</h5>
+                        <p class="text-left text-lg-center">
+                            Перевозку нашего рояля теперь я смогу доверить только вам, вашим грузчикам. Спасибо за аккуратную и профессиональную работу!
+                        </p>
+                        <div class="transportation-parameters">
+                            <p class="param">
+                                <i class="b-param">Заказывал перевозку рояля</i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-royalya/royal3.jpg" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Шаткий Константин</h5>
+                        <p class="text-left text-lg-center">
+                            Для нашего коллектива перевозка рояля с грузчиками по городу и области значит очень много. С остальным реквизитом гораздо проще. Но для рояля нам приходится использовать специальный транспорт. Мы решили воспользоваться услугами вашей фирмы. Все получилось на Ура! Благодарим от души!
+                        </p>
+                        <div class="transportation-parameters">
+                            <p class="param">
+                                <i class="b-param">Заказывал перевозку рояля</i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-royalya/royal4.jpg" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Королев Валерий</h5>
+                        <p class="text-left text-lg-center">
+                            Ваши грузчики выполнили работу спокойно, уверенно и, что самое главное, аккуратно. Знали бы они, сколько стоит такой рояль, какой это инструмент!
+                        </p>
+                        <div class="transportation-parameters">
+                            <p class="param">
+                                <i class="b-param">Заказывал перевозку рояля</i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <a class="_carousel-control carousel-control-prev" href="#recommendationCaption1" role="button" data-slide="prev">
+                <img src="/images/left-arrow-type1.png" alt="стрелка влево">
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="_carousel-control carousel-control-next" href="#recommendationCaption1" role="button" data-slide="next">
+                <img src="/images/right-arrow-type1.png" alt="стрелка вправо">
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <p class="_header-3 text-center">Фото</p>
+    <div class="container client-reviews">
+        <div class="row">
+            <div class="carousel-container mx-auto">
+                <div id="client-reviews" class="carousel slide mx-auto mt-4 mb-4">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <a href="/images/perevozka-royalya/royal-photo1.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo1.jpg" alt="Фото 1">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/perevozka-royalya/royal-photo2.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo2.jpg" alt="Фото 2">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/perevozka-royalya/royal-photo3.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo3.jpg" alt="Фото 3">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/perevozka-royalya/royal-photo4.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo4.jpg" alt="Фото 4">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/perevozka-royalya/royal-photo5.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo5.jpg" alt="Фото 5">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/perevozka-royalya/royal-photo6.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo6.jpg" alt="Фото 6">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/perevozka-royalya/royal-photo7.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/perevozka-royalya/royal-photo7.jpg" alt="Фото 7">
+                            </a>
+                        </div>
+                    </div>
+                    <ol class="carousel-indicators mx-auto">
+                        <li data-target="#client-reviews" data-slide-to="0" class="active"></li>
+                        <li data-target="#client-reviews" data-slide-to="1"></li>
+                        <li data-target="#client-reviews" data-slide-to="2"></li>
+                        <li data-target="#client-reviews" data-slide-to="3"></li>
+                        <li data-target="#client-reviews" data-slide-to="4"></li>
+                        <li data-target="#client-reviews" data-slide-to="5"></li>
+                        <li data-target="#client-reviews" data-slide-to="6"></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</main>
+
+
+<?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
+
