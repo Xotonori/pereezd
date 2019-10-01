adcb60259d3c53f7cc84bafc1da03175f1d0aaac
Xotonori <xotonori29011991@gmail.com>
2019-10-02 02:32:55 +0300

    сделал страницу грузчики на переезд

---
+++ gruzchiki-na-pereezd/index.php
@@ -0,0 +1,328 @@
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
+<meta name="description" itemprop="description" content="Профессиональные грузчики на переезд в СПБ по выгодным ценам."/>
+<meta name="keywords" itemprop="keywords" content="грузчики на переезд,заказать,цена,стоимость" />
+<title>Грузчики на переезд в Санкт-Петербурге</title>
+<link rel="canonical" href="https://pereezdy-spb.ru/gruzchiki-na-pereezd/"/>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
+<img class="sofa" src="/images/header/sofa.png" alt="диван">
+<img class="man" src="/images/header/man.png" alt="грузчик">
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
+<h1>Грузчики на переезд</h1>
+<div class="urgent-call">Срочный вызов за <span class="red-bold">15 минут</span></div>
+<button type="button" class="request-call" data-toggle="modal" data-target="#header-modal">Заказать грузчиков</button>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+
+<main class="gruzchiki-na-pereezd">
+    <!--////PAGE-DESCRIPTION/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="js-long-text container">
+        <div class="row">
+            <p class="col-12 paragraph">
+                Грузчики для переезда в Санкт-Петербурге – доступная услуга от специалистов нашей компании. Вы можете вызвать грузчиков срочно или договорится об организации переезда заблаговременно. Наши сотрудники выполнят любые разгрузочно-погрузочные работы. Они избавят вас от рутины и необходимости поднимать тяжести. Учитывая специфику и объемы имущества, мы можем предоставить любое количество опытных грузчиков.
+            </p>
+            <div class="col d-flex justify-content-end">
+                <button class="js-podrobno">
+                    <span>Подробно</span>
+                    <img src="/images/header/city-triangle.png" alt="треугольник">
+                </button>
+            </div>
+        </div>
+    </div>
+    <!--////Benefits/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-benefits">
+        <div class="row mb-4">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Почему <span class="important">доверяют</span> нашей компании</h2>
+            </div>
+        </div>
+        <div class="js-long-text container mb-5">
+            <div class="row">
+                <p class="col-12">
+                    Грузчики для переезда в Санкт-Петербурге – доступная услуга от специалистов нашей компании. Вы можете вызвать грузчиков срочно или договорится об организации переезда заблаговременно. Наши сотрудники выполнят любые разгрузочно-погрузочные работы. Они избавят вас от рутины и необходимости поднимать тяжести. Учитывая специфику и объемы имущества, мы можем предоставить любое количество опытных грузчиков.
+                </p>
+                <div class="col d-flex justify-content-end">
+                    <button class="js-podrobno">
+                        <span>Подробно</span>
+                        <img src="/images/header/city-triangle.png" alt="треугольник">
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 benefit1 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive1.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Адекватная стоимость услуг.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit2 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive2.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Соблюдение всех договоренностей.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit3 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive3.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Целостность и сохранность перевозимого имущества.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit4 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/belive4.png" alt="копилка">
+                    </div>
+                    <p class="h-hint col-12 text-center">Высокий уровень мотивации.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid header-main-theme discount-block mb-5">
+        <div class="row justify-content-center">
+            <div class="col-12 d-flex">
+                <img class="rullon mx-auto" src="/images/rullon.png" alt="рулон">
+            </div>
+            <h6 class="_header-6 col-11 text-center"><span class="important">Закажите услугу</span> по переезду магазина и получите скидку!</h6>
+            <span class="additional-info2 col-11 text-center mt-2">Скидка на упаковочный материал <br><span class="big-red">40%</span></span>
+            <div class="col-12 d-flex">
+                <!-- Modal -->
+                <button type="button" class="request-call mx-auto" data-toggle="modal" data-target="#header-modal">Заказать переезд</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-11 text-center">
+                <h3 class="_header-3 mt-3 mb-4">Мы оказываем <span class="important">все виды услуг</span></h3>
+            </div>
+            <div class="col-11">
+                <ul class="marker">
+                    <li>Погрузка, выгрузка тяжелых и крупногабаритных грузов.</li>
+                    <li>Доставка, установка, монтаж, демонтаж различного оборудования.</li>
+                    <li>Перемещение грузов в труднодоступных местах.</li>
+                </ul>
+            </div>
+            <div class="col-11 js-long-text container how-much-description">
+                <div class="row">
+                    <p class="col-12 ">
+                        Заказать грузчиков на переезд квартиры, офиса, предприятия или склада можно заранее. Опытный сотрудник прибудет по вашему адресу, и оценить объемы предстоящих работ. При необходимости мы аккуратно упакуем ваши личные вещи, мебель, оборудование и другое имущество. Обеспечим его фиксацию на транспортном средстве. Грузчики на переезд в Санкт-Петербурге помогут распаковать и расставить вещи на новом месте. Вы можете самостоятельно контролировать их работу или доверить переезд нашим специалистам.
+                    </p>
+                    <div class="col d-flex justify-content-end">
+                        <button class="js-podrobno">
+                            <span>Подробно</span>
+                            <img src="/images/header/city-triangle.png" alt="треугольник">
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <p class="_header-3 mt-3 mb-4">Мы оказываем <span class="important">все виды услуг</span></p>
+        </div>
+        <table class="tariff" style="margin-bottom: 30px;">
+            <tbody>
+            <tr class="tariff-title">
+                <td valign="top" width="308"><b>Виды работ</b></td>
+                <td valign="top" width="188"><b>Цена за работу 1 грузчика</b></td>
+                <td valign="top" width="92"><b>Мин. заказ</b></td>
+            </tr>
+            <tr>
+                <td width="308">Погрузка, разгрузка и складские работы</td>
+                <td width="188">От 300 до 350 руб в час</td>
+                <td width="92">5 часов</td>
+            </tr>
+            <tr>
+                <td width="308">Разборка, упаковка, погрузка, разгрузка, сборка, расстановка, демонтаж, монтаж (карнизы, люстры и т.д.)</td>
+                <td width="188">От 350 до 450 руб</td>
+                <td width="92">4 часа</td>
+            </tr>
+            <tr>
+                <td width="308">Такелажные работы (предметы свыше 100 кг:сейфы, пианино, банкоматы и т.п.)</td>
+                <td colspan="2" width="280">Стоимость рассчитывается индивидуально!</td>
+            </tr>
+            </tbody>
+        </table>
+        <p>Грузчики в СПБ недорого – звоните +7 (812) 982-88-58 или заполняйте форму обратной связи на сайте.</p>
+    </div>
+
+    <!--////KNOW-HOW-MUCH/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-benefits">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Как узнать <span class="important">стоимость на услуги</span> грузчиков</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 benefit1 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/price1.png" alt="копилка">
+                    </div>
+                    <a href="/calculyator" class="h-hint col-12 text-center">Рассчитать стоимость на калькуляторе</a>
+                    <p class="hint col-12 text-center">Мы можете самостоятельно
+                        за 5 секунд рассчитать стоимость
+                        переезда на нашем калькуляторе.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit2 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/price2.png" alt="копилка">
+                    </div>
+                    <a href="/zakaz" class="h-hint col-12 text-center">Вызвать оценщика (бесплатно)</a>
+                    <p class="hint col-12 text-center">Наш менеджер рассчитает точную
+                        стоимость переезда под ключ,
+                        которая потом не изменится.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 benefit3 benefit container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="/images/price3.png" alt="копилка">
+                    </div>
+                    <a href="/price" class="h-hint col-12 text-center">Посмотреть цены
+                        на наши услуги</a>
+                    <p class="hint col-12 text-center">Наш менеджер рассчитает точную
+                        стоимость переезда под ключ,
+                        которая потом не изменится.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--////QUESTION/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="person-page">
+        <div class="container-fluid header-main-theme person-page-price">
+            <p class="_header-3 mt-3 text-center"><span class="important">Остались вопросы</span> - спросите специалиста</p>
+            <img class="back-office" src="/images/back-office.png" alt="диван">
+        </div>
+        <div class="person-page-call">
+            <img class="man mx-auto" src="/images/manager.png" alt="менеджер">
+            <img class="man double-quotes mx-auto" src="/images/double-quotes.png" alt="менеджер">
+            <h6 class="_header-6 text-center">
+                Николай Денисов
+            </h6>
+            <div class="additional-info2 text-center mt-2">
+                Специализация: переезды
+                по Санкт-Петербургу "под ключ"<br>
+                +7 (812) 982-88-58
+            </div>
+            <div class="col-11 js-long-text container how-much-description">
+                <div class="row">
+                    <p class="col-12 paragraph text-left">
+                        Услуги грузчиков на переезд офиса, квартиры, склада помогут оптимизировать весь процесс. Благодаря усилиям наших сотрудников, ваше участие в переезде будет минимальным. Вы останетесь довольны нашей работой.
+                    </p>
+                    <div class="col d-flex justify-content-center">
+                        <button class="js-podrobno">
+                            <span>Подробно</span>
+                            <img src="/images/header/city-triangle.png" alt="треугольник">
+                        </button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 d-flex">
+                        <button type="button" class="request-call mx-auto" data-toggle="modal" data-target="#questionModal1">Задать вопрос</button>
+                    </div>
+                    <div class="modal fade" id="questionModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="questionModal1-title">Отправить свой вопрос</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <form action="" class="js-modal" id="questionProfile1__info">
+                                    <div class="modal-body">
+                                        <div class="form-group">
+                                            <label for="question1-name">Ваше имя</label>
+                                            <input type="text" class="form-control" name="name" data-id="name" id="question1-name"
+                                                   placeholder="Ваше имя">
+                                            <small class="text-danger"></small>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="question1-tel">Ваш телефон</label>
+                                            <input type="tel" class="form-control" name="tel" data-id="tel" id="question1-tel"
+                                                   placeholder="+79210000000">
+                                            <small class="text-danger"></small>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="question1-question">Ваш вопрос</label>
+                                            <textarea class="form-control" name="question" data-id="question" id="question1-question"
+                                                      placeholder="Ваш вопрос?"></textarea>
+                                            <small class="text-danger"></small>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="send-button">Отправить</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--////infographics////////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container-fluid wrap-infographics">
+        <img class="ribbon left-ribbon" src="/images/ribbon-left.png" alt="левая лента">
+        <img class="ribbon right-ribbon" src="/images/ribbon-right.png" alt="правая лента">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Переезд без хлопот в <span class="important">цифрах</span></h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-2 benefit text-center mt-4">
+                <p class="infographic">
+                    <span class="h-infographic"><span class="important">10</span><br>лет</span><br>
+                    <span class="b-infographic">на рынке услуг по организации переездов</span>
+                </p>
+            </div>
+            <div class="col-lg-2 benefit text-center mt-4">
+                <p class="infographic">
+                    <span class="h-infographic"><span class="important">24</span><br>часа</span><br>
+                    <span class="b-infographic">работаем круглосуточно</span>
+                </p>
+            </div>
+            <div class="col-lg-2 benefit text-center mt-4">
+                <p class="infographic">
+                    <span class="h-infographic"><span class="important">20</span><br>и более</span><br>
+                    <span class="b-infographic">переездов по Санкт-Петербургу в день</span>
+                </p>
+            </div>
+            <div class="col-lg-2 benefit text-center mt-4">
+                <p class="infographic">
+                    <span class="h-infographic"><span class="important">7</span><br>складов</span><br>
+                    <span class="b-infographic">где вы сможете хранить мебель</span>
+                </p>
+            </div>
+            <div class="col-lg-2 benefit text-center mt-4">
+                <p class="infographic">
+                    <span class="h-infographic"><span class="important">30</span><br>человек</span><br>
+                    <span class="b-infographic">штат своих грузчиков специалистов</span>
+                </p>
+            </div>
+            <div class="col-lg-2 benefit text-center mt-4">
+                <p class="infographic">
+                    <span class="h-infographic"><span class="important">10 000</span><br>коробок</span><br>
+                    <span class="b-infographic">+ стеллажи самый большой перевозимый нами архив</span>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+</main>
+
+<?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
+
