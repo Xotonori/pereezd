0282162ba30a56ff378d71e8e7bdb4a05987cc5c
Xotonori <xotonori29011991@gmail.com>
2019-10-02 06:05:58 +0300

    сделал страницу Перевозка плиты с грузчиками

---
+++ perevozka-plity/index.php
@@ -0,0 +1,157 @@
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
+<meta name="description" itemprop="description" content="Компания Переезд Без Хлопот в Санкт-Петербурге организует перевозку плиты с грузчиками."/>
+<meta name="keywords" itemprop="keywords" content="перевозка плиты с грузчиками Санкт-Петербург СПБ область недорого перевезти" />
+<title>Перевозка плиты с грузчиками по Санкт-Петербургу</title>
+<link rel="canonical" href="https://pereezdy-spb.ru/perevozka-plity/"/>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
+<img class="sofa" src="/images/header/sofa.png" alt="диван">
+<img class="man" src="/images/header/man.png" alt="грузчик">
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
+<h1>Перевозка плиты с грузчиками</h1>
+<div class="urgent-call">Срочный вызов за <span class="red-bold">15 минут</span></div>
+<button type="button" class="request-call" data-toggle="modal" data-target="#header-modal">Заказать перевозку</button>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+
+<main class="perevozka-plity">
+    <!--////PAGE-DESCRIPTION///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="js-long-text container">
+        <div class="row">
+            <p class="col-12 paragraph">
+                Плита – довольно ценная техника, поэтому так важно обеспечить его безопасность при транспортировке. Перевозка плиты с грузчиками нашей компании гарантированно защитит её от ударов и падений. Вы можете быть спокойны: плита доберётся до нужного места без происшествий. Наша компания возьмёт на себя и финансовую ответственность за перевозимую технику. Наличие специальных автомобилей с необходимыми габаритами позволяет транспортировать крупногабаритную технику с соблюдением норм безопасности.
+            </p>
+            <div class="col d-flex justify-content-end">
+                <button class="js-podrobno">
+                    <span>Подробно</span>
+                    <img src="/images/header/city-triangle.png" alt="треугольник">
+                </button>
+            </div>
+        </div>
+    </div>
+    <!--////garant///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-garant">
+        <h3 class="_header-3 text-center mb-4"><span class="important">Низкие</span> цены</h3>
+        <div class="row">
+            <div class="h-100 col-lg-3 d-flex justify-content-lg-start justify-content-center">
+                <img src="/images/garant.png" alt="гарантия">
+            </div>
+            <p class="garant col-lg-9">
+                У нас дешевле, чем у конкурентов, потому что мы имеем собственный автопарк, ежедневно выполняем множество заказов и заботимся о комфорте наших клиентов. Если вам нужна недорогая перевозка диванов по Санкт-Петербургу без лишних хлопот обратитесь к нам, и мы обещаем приятно удивить вас доступными ценами.
+            </p>
+        </div>
+    </div>
+
+
+    <!--////garant///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-kv-gorod">
+        <h2 class="_header-3 text-center mb-4">
+            <span class="important">Перевозка</span> с грузчиками<br>
+            <span class="red-bold">
+                от <span class="big-red">3800</span> руб.
+            </span>
+        </h2>
+        <div class="row">
+            <div class="col-lg-3 d-flex align-items-center flex-lg-wrap align-items-stretch mb-lg-0 mb-3">
+                <div class="col-lg-12 d-flex justify-content-center justify-content-lg-start wrap-check-mark">
+                    <div class="h-100 d-inline-block check-mark d-flex align-content-center">
+                        <img src="/images/gl.png" alt="галочка" class="mr-4">
+                    </div>
+                    <div class="d-inline-block">
+                        Газель фургон 10 м3
+                    </div>
+                </div>
+                <div class="col-lg-12 d-flex justify-content-center justify-content-lg-start wrap-check-mark">
+                    <div class="h-100 d-inline-block check-mark d-flex align-content-center">
+                        <img src="/images/gl.png" alt="галочка" class="mr-4">
+                    </div>
+                    <div class="d-inline-block">
+                        2 грузчика
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-9 d-flex align-items-center">
+                Перевозка плиты в СПб и области с грузчиками – очень востребованная услуга, ведь это массивная техника, справиться с погрузкой которой не так просто. Вам не понадобится спускать этот агрегат по лестнице или пытаться уместить в лифт - профессиональные грузчики сделают всё самостоятельно. Всего несколько минут – и плита погружена в автомобиль, а затем таким же образом поднята на нужный этаж. Удобно и без вашего участия!
+            </div>
+        </div>
+    </div>
+    <!--////RECOMMENDATION/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="recommendation container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Нас<span class="important"> рекомендуют</span></h2>
+            </div>
+        </div>
+    </div>
+    <div class="bd-example">
+        <div id="recommendationCaption1" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img src="/images/perevozka-plity/pp1.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Елена</h5>
+                        <p class="text-left text-lg-center">
+                            Отправляла старые вещи на дачу, решила воспользоваться перевозкой плиты. Не жалею: ребята забрали её прямо с кухни, мне не пришлось её двигать к двери. Ламинат на кухне тоже не поцарапали, довезли быстро, цена незаметная для кошелька.
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-plity/pp2.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Александр</h5>
+                        <p class="text-left text-lg-center">
+                            Когда возникла необходимость перевезти мою плиту на новую съёмную квартиру, решил вызвать грузчиков, потому что своей машины нет. Прислали авто с грузчиками, которые сами забрали мою плиту и спустили с четвёртого этажа без лифта, сами же и занесли на новое место. Рекомендую такую перевозку плиты, потому что я сэкономил время и силы, а главное, техника в сохранности.
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-plity/pp3.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Яна</h5>
+                        <p class="text-left text-lg-center">
+                            Грузчики, чтобы перевезти плиту – хороший выход, когда не хочется таскать тяжести самой и некого попросить. Моя мама пенсионерка, я вызвала ей грузчиков вместе с машиной. Не пришлось ни её тревожить, ни мне приезжать и в этом участвовать, грузчики всё сделали сами и не поцарапали ничего.
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-plity/pp4.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Даниил</h5>
+                        <p class="text-left text-lg-center">
+                            Решил перевезти плиту в Санкт-Петербурге цивилизованным методом: с привлечением грузчиков. Ни капли не пожалел, услуга стоит недорого, и не надо переживать, что плита упадёт и разобьётся, потому что компания за него отвечает финансово. В моём случае ничего не разбили, а доставили всё в целости, за что спасибо!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <a class="_carousel-control carousel-control-prev" href="#recommendationCaption1" role="button" data-slide="prev">
+                <img src="/images/left-arrow-type1.png" alt="стрелка влево">
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="_carousel-control carousel-control-next" href="#recommendationCaption1" role="button" data-slide="next">
+                <img src="/images/right-arrow-type1.png" alt="стрелка вправо">
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <!--////DISCOUNT/////////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container-fluid header-main-theme _call-to-action mb-5 h-auto">
+        <div class="row justify-content-center">
+            <h3 class="_header-3 text-center mb-3">
+                <span class="important">Закажите </span>
+                перевозку
+                <span class="important"> плиты </span>
+                сейчас!
+            </h3>
+            <div class="w-100 text-center  mb-3">Это можно сделать по телефону или заполнив онлайн заявку на нашем сайте.</div>
+            <a href="tel:+79618018881" class="text-center big-red">+7 (961) 801-88-81</a>
+
+
+            <div class="col-12 d-flex">
+                <!-- Modal -->
+                <button type="button" class="request-call mx-auto" data-toggle="modal" data-target="#header-modal">Заказать переезд</button>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
