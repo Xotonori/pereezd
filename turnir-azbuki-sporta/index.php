80df194fa0ef4833b815dfeec0bb515c729acabf
Dmitrieva.GG <dmitrieva.gg@parma.ru>
2019-10-03 02:09:43 +0300

    page turnir-azbuki-sporta

---
+++ turnir-azbuki-sporta/index.php
@@ -0,0 +1,221 @@
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
+<meta name="description" itemprop="description" content="19 и 20 сентября — «ВЕСЁЛЫЕ РЕБЯТА» в Санкт-Петербурге! 19-20 сентября на стадионе «Динамо» прошел ежегодный международный турнир среди детских команд" />
+<meta name="keywords" itemprop="keywords" content="" />
+<title>Турнир Азбуки спорта</title>
+<link rel="canonical" href="https://pereezdy-spb.ru/turnir-azbuki-sporta/" />
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
+<!--<img class="sofa" src="/images/header/sofa.png" alt="диван">-->
+<!--<img class="man" src="/images/header/man.png" alt="грузчик">-->
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
+<h1>Турнир Азбуки спорта</h1>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+
+<main class="turnir">
+    <!--////PAGE-DESCRIPTION/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-11 text-center mb-3">
+                <h2 class="_header-3 mt-3">19 и 20 сентября — <span class="important">«ВЕСЁЛЫЕ РЕБЯТА»</span> в Санкт-Петербурге!</h2>
+            </div>
+            <div class="col-12 text-center">
+                <img class="mw-100" src="/images/ssbg.jpg" alt="Турнир «ВЕСЁЛЫЕ РЕБЯТА» в Санкт-Петербурге!">
+            </div>
+            <div class="col-11 js-long-text container how-much-description mt-4">
+                <div class="row">
+                    <div class="col-12 paragraph pt-0">
+                        <p>19-20 сентября на стадионе «Динамо» прошел ежегодный международный турнир среди детских команд «Весёлые Ребята-2015». Свидетелями данного праздника стали свыше 3000 человек. Всё внимание было приковано к юным футболистам, которые в течение двух дней сражались за самые высокие места. Турнир прошел в 4-ех возрастных категориях: 2006, 2007, 2008 и 2009 года рождения.</p>
+                        <p>Наша компания приняла активное участие в организации данного мероприятия и с полной ответственностью отнеслась к перевозке имущества. Все, что необходимо было для праздника, доставили в срок и без повреждений.</p>
+                        <p>Было приятно не только перевозить, но также и болеть за детей непосредственно во время футбольного турнира. Команд оказалось настолько много, что наши сотрудники стали болельщиками, выбрав себе по одной. Мы уже между собой соревновались, кто же победит в турнире.</p>
+                        <p>Очень приятно работать с сотрудниками компании «Азбука спорта» и решать поставленные задачи. Возникавшие проблемы легко и мобильно исправлялись путем совместных договоренностей.</p>
+                        <p>Надеемся на долгосрочное сотрудничество и на товарищеский матч!</p>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <button class="js-podrobno">
+                            <span>Подробно</span>
+                            <img src="/images/header/city-triangle.png" alt="треугольник">
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--////photos////////////////////////////////////////////////////////////////////////////////////////////////-->
+    <h2 class="_header-3 text-center">Фотогалерея</h2>
+    <div class="container client-reviews">
+        <div class="row">
+            <div class="carousel-container mx-auto">
+                <div id="client-reviews" class="carousel slide mx-auto mt-4 mb-4">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <a href="/images/turnir-azbuki-sporta/photo1.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo1_mini.jpg" alt="Фото 1">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo2.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo2_mini.jpg" alt="Фото 2">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo3.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo3_mini.jpg" alt="Фото 3">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo4.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo4_mini.jpg" alt="Фото 4">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo5.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo5_mini.jpg" alt="Фото 5">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo6.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo6_mini.jpg" alt="Фото 6">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo7.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo7_mini.jpg" alt="Фото 7">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo8.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo8_mini.jpg" alt="Фото 8">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo9.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo9_mini.jpg" alt="Фото 9">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo10.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo10_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo11.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo11_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo12.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo12_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo13.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo13_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo14.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo14_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo15.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo15_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo16.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo16_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo17.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo17_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo18.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo18_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo19.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo19_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo20.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo20_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo21.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo21_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo22.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo22_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo23.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo23_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo24.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo24_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo25.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo25_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo26.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo26_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="/images/turnir-azbuki-sporta/photo27.jpg" data-toggle="lightbox" data-gallery="review-photo">
+                                <img class="d-block" src="/images/turnir-azbuki-sporta/photo27_mini.jpg" alt="Фото 10">
+                            </a>
+                        </div>
+                    </div>
+                    <ol class="carousel-indicators mx-auto">
+                        <li data-target="#client-reviews" data-slide-to="0" class="active"></li>
+                        <li data-target="#client-reviews" data-slide-to="1"></li>
+                        <li data-target="#client-reviews" data-slide-to="2"></li>
+                        <li data-target="#client-reviews" data-slide-to="3"></li>
+                        <li data-target="#client-reviews" data-slide-to="4"></li>
+                        <li data-target="#client-reviews" data-slide-to="5"></li>
+                        <li data-target="#client-reviews" data-slide-to="6"></li>
+                        <li data-target="#client-reviews" data-slide-to="7"></li>
+                        <li data-target="#client-reviews" data-slide-to="8"></li>
+                        <li data-target="#client-reviews" data-slide-to="9"></li>
+                        <li data-target="#client-reviews" data-slide-to="10"></li>
+                        <li data-target="#client-reviews" data-slide-to="11"></li>
+                        <li data-target="#client-reviews" data-slide-to="12"></li>
+                        <li data-target="#client-reviews" data-slide-to="13"></li>
+                        <li data-target="#client-reviews" data-slide-to="14"></li>
+                        <li data-target="#client-reviews" data-slide-to="15"></li>
+                        <li data-target="#client-reviews" data-slide-to="16"></li>
+                        <li data-target="#client-reviews" data-slide-to="17"></li>
+                        <li data-target="#client-reviews" data-slide-to="18"></li>
+                        <li data-target="#client-reviews" data-slide-to="19"></li>
+                        <li data-target="#client-reviews" data-slide-to="20"></li>
+                        <li data-target="#client-reviews" data-slide-to="21"></li>
+                        <li data-target="#client-reviews" data-slide-to="22"></li>
+                        <li data-target="#client-reviews" data-slide-to="23"></li>
+                        <li data-target="#client-reviews" data-slide-to="24"></li>
+                        <li data-target="#client-reviews" data-slide-to="25"></li>
+                        <li data-target="#client-reviews" data-slide-to="26"></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
+
