213a6607367a06441cc2fbd0cb0a04b7241c832c
Xotonori <xotonori29011991@gmail.com>
2019-10-02 05:31:18 +0300

    сделал страницу Перевозка мебели на дачу с грузчиками

---
+++ perevozka-mebeli-na-dachu/index.php
@@ -0,0 +1,157 @@
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part1.php'); ?>
+<meta name="description" itemprop="description" content="Компания Переезд Без Хлопот в Санкт-Петербурге организует перевозку мебели на дачу с грузчиками."/>
+<meta name="keywords" itemprop="keywords" content="перевозка мебели на дачу с грузчиками Санкт-Петербург СПБ область недорого перевезти" />
+<title>Перевозка мебели на дачу с грузчиками по Санкт-Петербургу</title>
+<link rel="canonical" href="https://pereezdy-spb.ru/perevozka-mebeli-na-dachu/"/>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part2.php'); ?>
+<img class="sofa" src="/images/header/sofa.png" alt="диван">
+<img class="man" src="/images/header/man.png" alt="грузчик">
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part3.php'); ?>
+<h1>Перевозка мебели на дачу с грузчиками</h1>
+<div class="urgent-call">Срочный вызов за <span class="red-bold">15 минут</span></div>
+<button type="button" class="request-call" data-toggle="modal" data-target="#header-modal">Заказать перевозку</button>
+<?php include_once  ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header' . DIRECTORY_SEPARATOR . 'header-part4.php'); ?>
+
+<main class="perevozka-mebeli-na-dachu">
+    <!--////PAGE-DESCRIPTION///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="js-long-text container">
+        <div class="row">
+            <p class="col-12 paragraph">
+                Перевозка мебели на дачу с грузчиками – удобное решение для экономии массы времени и сил. Если вам нужно отправить диван, кресло, шкаф, даже антикварный комод или любую другую мебель на дачу, заказывайте такую услугу у профессионалов. Вы сами оцените все выгоды такого варианта: быстроту доставки, отсутствие повреждений у перевозимой мебели и то, что вам не придётся переживать за её сохранность. Ведь перевозчик берёт на себя полную материальную ответственность за груз и сроки его доставки. Пользуясь такой услугой, вы сохраняете не только мебель, но и свою нервную систему.
+            </p>
+            <div class="col d-flex justify-content-end">
+                <button class="js-podrobno">
+                    <span>Подробно</span>
+                    <img src="/images/header/city-triangle.png" alt="треугольник">
+                </button>
+            </div>
+        </div>
+    </div>
+    <!--////garant///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-garant">
+        <h3 class="_header-3 text-center mb-4"><span class="important">Гарантируем</span> низкие цены</h3>
+        <div class="row">
+            <div class="h-100 col-lg-3 d-flex justify-content-lg-start justify-content-center">
+                <img src="/images/garant.png" alt="гарантия">
+            </div>
+            <p class="garant col-lg-9">
+                Недорогая перевозка мебели на дачу из Санкт-Петербурга доступна каждому. Мы сделали всё для того, чтобы наши услуги были вам по карману. Обращение к нам выгодно, так как грузчиков и автомобиль вы заказываете в одном месте. А благодаря собственному автопарку мы ещё уменьшили цены. Убедитесь сами: более дешёвых перевозок вы не найдёте в городе!
+            </p>
+        </div>
+    </div>
+
+
+    <!--////garant///////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container wrap-kv-gorod">
+        <h2 class="_header-3 text-center mb-4">
+            <span class="important">Профессиональные</span> грузчики<br>
+            <span class="red-bold">
+                от <span class="big-red">3800</span> руб.
+            </span>
+        </h2>
+        <div class="row">
+            <div class="col-lg-3 d-flex align-items-center flex-lg-wrap align-items-stretch mb-lg-0 mb-3">
+                <div class="col-lg-12 d-flex justify-content-center justify-content-lg-start wrap-check-mark">
+                    <div class="h-100 d-inline-block check-mark d-flex align-content-center">
+                        <img src="/images/gl.png" alt="галочка" class="mr-4">
+                    </div>
+                    <div class="d-inline-block">
+                        Газель фургон 10 м3
+                    </div>
+                </div>
+                <div class="col-lg-12 d-flex justify-content-center justify-content-lg-start wrap-check-mark">
+                    <div class="h-100 d-inline-block check-mark d-flex align-content-center">
+                        <img src="/images/gl.png" alt="галочка" class="mr-4">
+                    </div>
+                    <div class="d-inline-block">
+                        2 грузчика
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-9 d-flex align-items-center">
+                Не хотите сами перетаскивать много тяжёлых столов и диванов? Перевозка мебели с грузчиками на дачу – то, что вам нужно! Наши специалисты имеют навыки сборки-разборки, бережного обращения с мебелью и быстрой транспортировки даже очень громоздких вещей. Обращайтесь, чтобы осуществить переезд мебели на дачу без всяких усилий с вашей стороны.
+            </div>
+        </div>
+    </div>
+    <!--////RECOMMENDATION/////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="recommendation container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 text-center">
+                <h2 class="_header-3">Нас<span class="important"> рекомендуют</span></h2>
+            </div>
+        </div>
+    </div>
+    <div class="bd-example">
+        <div id="recommendationCaption1" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img src="/images/perevozka-mebeli-na-dachu/pmnd1.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Григорий</h5>
+                        <p class="text-left text-lg-center">
+                            При очередной поездке за город мне нужны былигрузчики, чтобы перевезти мебель на дачу. Мне рекомендовал эту услугу приятель, который всегда возит габаритную мебель таким образом. Для меня это был первый опыт, но всё прошло хорошо, не пришлось думать, как погрузить большой стол на легковушку. Я заказал перевозку один раз и отвёз всю нужную мебель, чтобы не мучиться целый сезон с поштучной транспортировкой
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-mebeli-na-dachu/pmnd2.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Ксения</h5>
+                        <p class="text-left text-lg-center">
+                            Мы с мужем заказываем перевозку мебелиза город и обратно два раза в год. На зиму оставлять не хочется– может испортиться. Заодно часто перевозим урожай. Всем рекомендую машину с грузчиками: удобно и не нужно таскать тяжести самому. Смотришь, как работают другие, а сама отдыхаешь.
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-mebeli-na-dachu/pmnd3.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Егор</h5>
+                        <p class="text-left text-lg-center">
+                            С перевозкой вещей и мебели на дачу становится намного проще всё туда отправить и не таскать вещи с этажа в машину. Стоимость небольшая, лишний раз гонять свою машину примерно так же и выйдет. А здесь не нужно напрягаться: я сказал, что нужно отвезти, грузчики всё унесли и перевезли. Удобный современный формат.
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/perevozka-mebeli-na-dachu/pmnd4.png" class="d-block mx-auto" alt="слайд">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Мария</h5>
+                        <p class="text-left text-lg-center">
+                            Меня соседка попросила перевезти диван на дачу, который мне был не нужен, и отдать ей. Она сама заказала перевозку, ко мне приехала Газель с грузчиками. Они всё забрали и увезли, аккуратно спустили диван на улицу, я только смотрела за процессом. Все остались довольны результатом.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <a class="_carousel-control carousel-control-prev" href="#recommendationCaption1" role="button" data-slide="prev">
+                <img src="/images/left-arrow-type1.png" alt="стрелка влево">
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="_carousel-control carousel-control-next" href="#recommendationCaption1" role="button" data-slide="next">
+                <img src="/images/right-arrow-type1.png" alt="стрелка вправо">
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <!--////DISCOUNT/////////////////////////////////////////////////////////////////////////////////////////////////-->
+    <div class="container-fluid header-main-theme _call-to-action mb-5 h-auto">
+        <div class="row justify-content-center">
+            <h3 class="_header-3 text-center mb-3">
+                <span class="important">Закажите </span>
+                перевозку мебели
+                <span class="important"> на дачу </span>
+                сейчас!
+            </h3>
+            <div class="w-100 text-center  mb-3">Это можно сделать по телефону или заполнив онлайн заявку на нашем сайте.</div>
+            <a href="tel:+79618018881" class="text-center big-red">+7 (961) 801-88-81</a>
+
+
+            <div class="col-12 d-flex">
+                <!-- Modal -->
+                <button type="button" class="request-call mx-auto" data-toggle="modal" data-target="#header-modal">Заказать переезд</button>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php include_once ($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer' . DIRECTORY_SEPARATOR . 'footer.php'); ?>
